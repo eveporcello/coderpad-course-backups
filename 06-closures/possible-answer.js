a7c380599092ebb53f45bd1265ba4b3598013754
eveporcello <eve@moonhighway.com>
2023-06-04 15:44:12 -0700

    Adds closures notes

--- 06-closures/possible-answer.js
+++ 06-closures/possible-answer.js
@@ -1,3 +1,10 @@
+// A closure gives you access to an outer functions scope from an
+// inner function.
+
+// congratulate returns the inner function: createSentence
+// This function has access to the sentence var from the congratulate function
+// The create sentence function closes over the sentence variable
+
 function congratulate(user) {
   let sentence = "Great job";
 
@@ -7,3 +14,8 @@ function congratulate(user) {
 
   return createSentence;
 }
+
+const firstName = "Jenny";
+const congratulations = congratulate(firstName);
+const result = congratulations();
+console.log(result);
